45eb98f2a1e34147516b7016cef22c9ce6660aa3
[mick lito] <[micklito.dev@gmail.com]>
2023-01-31 15:02:45 -0500

    better formated data in main.js by using an algorithm to convert the temprature from kelvins to farenheight then used math.floor to remove the extensively long decimal

--- src/main.js
+++ src/main.js
@@ -7,14 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const endpoint = "https://api.openweathermap.org/";
   const apiKey = "6f9ea20be5ffff67f64770cd3b4ad14c";
 
-
-
   const lat = 33.74;
   const lon = -84.38;
-
-
- 
-
 
   const apiEngine = async () => {
     const path = "data/2.5/forecast";
@@ -26,23 +20,26 @@ document.addEventListener("DOMContentLoaded", function () {
       if (response.ok) {
         let jsonResponse = await response.json();
         console.log(jsonResponse);
-        let status = jsonResponse.list[0].weather[0].main
-        
-        if(status === 'Clouds') {
-          var state = '☁️'
-        } else if (status === 'Sun') {
-            var state =  '🔆' 
-        } else if (status === 'Rain') {
-             var state = '🌧️'
-        } else if (status === 'partly cloudy') {
-              var state = '⛅'
+        let status = jsonResponse.list[0].weather[0].main;
+
+        if (status === "Clouds") {
+          var state = "☁️";
+        } else if (status === "Sun") {
+          var state = "🔆";
+        } else if (status === "Rain") {
+          var state = "🌧️";
+        } else if (status === "partly cloudy") {
+          var state = "⛅";
         }
 
-        title.textContent = `${jsonResponse.city.name} ${state}`
-        temp.textContent = `Temp: ${jsonResponse.list[0].main.temp}`
-        wind.textContent = `Wind: ${jsonResponse.list[0].wind.speed}`
-        humidity.textContent = ` Humidity: ${jsonResponse.list[0].main.humidity}`
+        let kelTemp = jsonResponse.list[0].main.temp;
+        let f = ((kelTemp - 273) * 9) / 5 + 32;
+        f = Math.floor(f);
 
+        title.textContent = `${jsonResponse.city.name} ${state}`;
+        temp.textContent = `Temp: ${f}F°`;
+        wind.textContent = `Wind: ${jsonResponse.list[0].wind.speed} MPH`;
+        humidity.textContent = ` Humidity: ${jsonResponse.list[0].main.humidity}%`;
       }
     } catch (Error) {
       console.log(Error);
