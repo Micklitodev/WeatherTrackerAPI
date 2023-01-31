e5c800d80d2a0b1584c5c3174efae821db88848a
[mick lito] <[micklito.dev@gmail.com]>
2023-01-31 14:23:46 -0500

    Updated main.js to properly display temp, wind, humidity, also added a conditional that checks if a specified city has the value of cloudy, rainy, sunny, if so it assigns the state variable a value of an emjoi then uses string interpolation to set it beside the city. Wrapped entire main.js file inside a event listener to check if html page has finished loading. updated css so that the cards of the 5day forecast were colored correctly.

--- src/main.js
+++ src/main.js
@@ -1,27 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let title = document.getElementById("test");
+  let temp = document.getElementById("test2");
+  let wind = document.getElementById("test3");
+  let humidity = document.getElementById("test4");
 
-const endpoint = 'https://api.openweathermap.org/'
-const apiKey = '6f9ea20be5ffff67f64770cd3b4ad14c'
-
-const lat = 44.34;
-const lon = 10.99; 
+  const endpoint = "https://api.openweathermap.org/";
+  const apiKey = "6f9ea20be5ffff67f64770cd3b4ad14c";
 
 
-const apiEngine = async () => {
-    const path = 'data/2.5/forecast'
-    const queryParams = `?lat=${lat}&lon=${lon}&appid=${apiKey}`
-    const url = `${endpoint}${path}${queryParams}`
 
-    try{
+  const lat = 33.74;
+  const lon = -84.38;
 
-        const response = await fetch(url)
-        if(response.ok) {
-            let jsonResponse = response.json(); 
-            console.log(jsonResponse)
+  const apiEngine = async () => {
+    const path = "data/2.5/forecast";
+    const queryParams = `?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const url = `${endpoint}${path}${queryParams}`;
+
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        let status = jsonResponse.list[0].weather[0].main
+        
+        if(status === 'Clouds') {
+          var state = '☁️'
+        } else if (status === 'Sun') {
+            var state =  '🔆' 
+        } else if (status === 'Rain') {
+             var state = '🌧️'
+        } else if (status === 'partly cloudy') {
+              var state = '⛅'
         }
 
-    }catch(Error) {
-        console.log(Error)
-    }
-}
+        title.textContent = `${jsonResponse.city.name} ${state}`
+        temp.textContent = `Temp: ${jsonResponse.list[0].main.temp}`
+        wind.textContent = `Wind: ${jsonResponse.list[0].wind.speed}`
+        humidity.textContent = ` Humidity: ${jsonResponse.list[0].main.humidity}`
 
-// apiEngine()
+      }
+    } catch (Error) {
+      console.log(Error);
+    }
+  };
+
+  apiEngine();
+});
