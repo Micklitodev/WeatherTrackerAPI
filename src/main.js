24425230525a3e445f36e1e6e01ab13603051c0b
[mick lito] <[micklito.dev@gmail.com]>
2023-01-31 18:13:57 -0500

    Updated index.html for the section tags child elements to have unique classes from main and changed card-tile to card-date since they will use a date instead of a title. updated main.js to use geological api to change variables of lat and lon and then recall the card classes with updated long lat. added an event listner to the click button. which executes geological api with a parameter of city. added a snow parameter and emoji for if it is snowing it will display an icon instead of undefined.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let title = document.getElementById("test");
-  let temp = document.getElementById("test2");
-  let wind = document.getElementById("test3");
-  let humidity = document.getElementById("test4");
+  const submitbtn = document.getElementById("submit");
+
+  let title = document.getElementById("main-card-title");
+  let temp = document.getElementById("main-card-temp");
+  let wind = document.getElementById("main-card-wind");
+  let humidity = document.getElementById("main-card-humidity");
 
   const endpoint = "https://api.openweathermap.org/";
   const apiKey = "6f9ea20be5ffff67f64770cd3b4ad14c";
 
-  const lat = 33.74;
-  const lon = -84.38;
+  let lat = 33.74;
+  let lon = -84.38;
 
   const apiEngine = async () => {
     const path = "data/2.5/forecast";
@@ -19,32 +21,108 @@ document.addEventListener("DOMContentLoaded", function () {
       const response = await fetch(url);
       if (response.ok) {
         let jsonResponse = await response.json();
-        console.log(jsonResponse);
-        let status = jsonResponse.list[0].weather[0].main;
-
-        if (status === "Clouds") {
-          var state = "☁️";
-        } else if (status === "Sun") {
-          var state = "🔆";
-        } else if (status === "Rain") {
-          var state = "🌧️";
-        } else if (status === "partly cloudy") {
-          var state = "⛅";
-        }
-
-        let kelTemp = jsonResponse.list[0].main.temp;
-        let f = ((kelTemp - 273) * 9) / 5 + 32;
-        f = Math.floor(f);
-
-        title.textContent = `${jsonResponse.city.name} ${state}`;
-        temp.textContent = `Temp: ${f}F°`;
-        wind.textContent = `Wind: ${jsonResponse.list[0].wind.speed} MPH`;
-        humidity.textContent = ` Humidity: ${jsonResponse.list[0].main.humidity}%`;
+        return jsonResponse;
       }
     } catch (Error) {
       console.log(Error);
     }
   };
 
-  apiEngine();
+  const mainCard = async () => {
+    let jsonResponse = await apiEngine();
+    let status = jsonResponse.list[0].weather[0].main;
+    console.log(jsonResponse);
+
+    if (status === "Clouds") {
+      var state = "☁️";
+    } else if (status === "Clear") {
+      var state = "🔆";
+    } else if (status === "Rain") {
+      var state = "🌧️";
+    } else if (status === "partly cloudy") {
+      var state = "⛅";
+    } else if (status === "Snow") {
+      var state = "❄️";
+    }
+
+    let kelTemp = jsonResponse.list[0].main.temp;
+    let f = ((kelTemp - 273) * 9) / 5 + 32;
+    f = Math.floor(f);
+
+    title.textContent = `${jsonResponse.city.name} ${state}`;
+    temp.textContent = `Temp: ${f}F°`;
+    wind.textContent = `Wind: ${jsonResponse.list[0].wind.speed} MPH`;
+    humidity.textContent = ` Humidity: ${jsonResponse.list[0].main.humidity}%`;
+  };
+  mainCard();
+
+  const sectionCards = async () => {
+    let section = document.getElementById("section");
+
+    let sectionTitle = document.querySelectorAll(".card-date");
+    let sectionTemp = document.querySelectorAll(".section-card-temp");
+    let sectionWind = document.querySelectorAll(".section-card-wind");
+    let sectionHumidity = document.querySelectorAll(".section-card-humidity");
+
+    let jsonResponse = await apiEngine();
+    let status = jsonResponse.list[0].weather[0].main;
+
+    if (status === "Clouds") {
+      var state = "☁️";
+    } else if (status === "Clear") {
+      var state = "🔆";
+    } else if (status === "Rain") {
+      var state = "🌧️";
+    } else if (status === "partly cloudy") {
+      var state = "⛅";
+    } else if (status === "Snow") {
+      var state = "❄️";
+    }
+
+    for (let i = 0; i < section.childElementCount; i++) {
+      let kelTemp = jsonResponse.list[i].main.temp;
+      let f = ((kelTemp - 273) * 9) / 5 + 32;
+      f = Math.floor(f);
+
+      sectionTitle[i].textContent = `${jsonResponse.list[i].dt} ${state}`;
+      sectionTemp[i].textContent = `Temp: ${f}F°`;
+      sectionWind[
+        i
+      ].textContent = `Wind: ${jsonResponse.list[i].wind.speed} MPH`;
+      sectionHumidity[
+        i
+      ].textContent = `Humidity: ${jsonResponse.list[i].main.humidity}%`;
+    }
+  };
+
+  sectionCards();
+
+  function handleClick(e) {
+    e.preventDefault();
+    const city = document.getElementById("city").value;
+    geologicalApi(city);
+  }
+
+  submitbtn.addEventListener("click", handleClick);
+
+  const geologicalApi = async (city) => {
+    let cityname = city;
+    console.log(cityname);
+    const path = "geo/1.0/direct";
+    const queryParams = `?q=${cityname}&limit=1&appid=${apiKey}`;
+    const url = `${endpoint}${path}${queryParams}`;
+
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        let jsonResponse = await response.json();
+        lat = jsonResponse[0].lat;
+        lon = jsonResponse[0].lon;
+        mainCard();
+        sectionCards();
+      }
+    } catch (Error) {
+      console.log(Error);
+    }
+  };
 });
